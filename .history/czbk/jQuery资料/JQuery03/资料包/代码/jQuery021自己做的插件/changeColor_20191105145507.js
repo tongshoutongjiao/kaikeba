411d76f8a08e8561fbc020a8687d51e8885a3477
202369 <1741969637@qq.com>
2019-11-08 09:43:46 +0800

    1108

---
+++ .history/czbk/jQuery资料/JQuery03/资料包/代码/jQuery021自己做的插件/changeColor_20191105145507.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2016/12/9.
+ */
+$.fn.changeBackgrounColor=function (color) {
+    console.log('哈哈哈');
+    $(".cls").css("backgroundColor",color);
+};
---
+++ .history/czbk/jQuery资料/JQuery03/资料包/代码/jQuery021自己做的插件/changeColor_20191105145657.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/12/9.
+ */
+$.fn.changeBackgrounColor=function (color) {
+    console.log('哈哈哈');
+
+    //  获取到当前元素
+    console.log($(this));
+    $(this).css("backgroundColor",color);
+};
