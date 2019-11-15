4ed2da88a6cf43c62d02ad7d1c8bb89b3e574d57
202369 <1741969637@qq.com>
2019-11-15 18:42:22 +0800

    add demo

---
+++ .history/czbk/H5项目和新增API资料/myDemo/js/myPage_20191111155613.js
@@ -0,0 +1,11 @@
+/*
+* @Author: apple
+* @Date:   2017-09-17 18:16:20
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2019-11-11 15:56:12
+*/
+$(function(){
+
+	$('#fullpage').fullpage();
+
+});
---
+++ .history/czbk/H5项目和新增API资料/myDemo/js/myPage_20191111161040.js
@@ -0,0 +1,12 @@
+/*
+* @Author: apple
+* @Date:   2017-09-17 18:16:20
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2019-11-11 15:58:00
+*/
+$(function(){
+
+	$('#fullpage').fullpage({
+		navigation:true
+	});
+});
